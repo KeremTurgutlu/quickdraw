1f87e17f930451b5c0e5940b719d125ec621502d
Ubuntu <ubuntu@ip-172-31-5-88.us-west-2.compute.internal>
2018-11-20 02:45:55 +0000

    mobilenet

--- Sketch-a-Net.py
+++ Sketch-a-Net.py
@@ -5,39 +5,35 @@ from fastai import *
 from fastai.vision import *
 from utils import *
 
-NUM_SAMPLES_PER_CLASS = 100000
 NUM_VAL = 50 * 340
 
 PATH = Path('../data/quickdraw/')
 
-bs = 400
+bs = 800
 sz = 256
-
-dfs_combined = pd.read_csv(PATH/"train/dfs_combined.csv")
 test_df = pd.read_csv(PATH/"test_simplified.csv")
-dfs_combined = dfs_combined[['drawing', 'word']]
 
 def create_func(item):
-    img = list2multichannel(eval(item[0]), size=225)
+    with open(item) as f: item = f.read()
+    img = list2multichannel(eval(item), size=225)
     tensor = torch.from_numpy(img).float()
     return tensor.div_(255)
 
-item_list = ItemList.from_df(dfs_combined, cols=['drawing', 'word'], create_func=create_func)
+item_list = ItemList.from_folder(PATH/"train_folders", create_func=create_func)
 np.random.seed(42)
 idxs = np.arange(item_list.items.shape[0])
 np.random.shuffle(idxs)
 val_idxs = idxs[:NUM_VAL]
 item_lists = item_list.split_by_idx(val_idxs)
-label_lists = item_lists.label_from_df(cols=1)
+label_lists = item_lists.label_from_folder()
 test_items = ItemList.from_df(test_df, cols=['drawing', 'key_id'], create_func=create_func)
 label_lists.add_test(test_items);
 
-train_dl = DataLoader(label_lists.train, bs, True)
-valid_dl = DataLoader(label_lists.valid, bs, False)
-test_dl = DataLoader(label_lists.test, bs, False)
+train_dl = DataLoader(label_lists.train, bs, True, num_workers=8)
+valid_dl = DataLoader(label_lists.valid, bs, False, num_workers=8)
+test_dl = DataLoader(label_lists.test, bs, False, num_workers=8)
 
 data_bunch = DataBunch(train_dl, valid_dl, test_dl)
-
 
 def conv_layer(ni:int, nf:int, ks:int=3, stride:int=1, pad=0)->nn.Sequential:
     "Create Conv2d->BatchNorm2d->LeakyReLu layer: `ni` input, `nf` out filters, `ks` kernel, `stride`:stride."
